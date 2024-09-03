bf70f47640a9600d4fbc399132b0ea9123443971
canaydindev <canaydindev@gmail.com>
2024-09-04 01:31:41 +0300

    v1702

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: "It's a medical hepl site",
 };
 import Providers from "./providers";
-import Navbar from "@/components/navbar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 import Container from "@/components/global/Container";
 
 export default function RootLayout({
@@ -19,13 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          <Navbar />
-          <Container className="py-10">{children}</Container>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers>
+            <Navbar />
+            <Container className="py-10">{children}</Container>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import Link from "next/link";
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "Logout oldunuz..." });
+  };
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton>
+      <Link
+        href="/"
+        className="w-full text-left capitalize"
+        onClick={handleLogout}
+      >
+        çıkış
+      </Link>
+    </SignOutButton>
+  );
 }
-
-export default SignOutLink
+export default SignOutLink;
